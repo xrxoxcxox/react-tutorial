36ba8daa2eba302e583c755fffab40942c4508c7
xrxoxcxox <watanuki@ju-zen.com>
2021-01-25 00:35:03 +0900

    Change Board component from class to function

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,4 +1,3 @@
-import { Component } from 'react';
 import { ISquare } from '../interface';
 import Square from './Square'
 
@@ -7,36 +6,34 @@ interface BoardProps {
     onClick: (i: number) => void;
 }
 
-class Board extends Component<BoardProps> {
-  renderSquare(i: number) {
+function Board(props: BoardProps) {
+  const renderSquare = (i: number) => {
     return (
       <Square
-        value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}
+        value={props.squares[i]}
+        onClick={() => props.onClick(i)}
       />
     );
   }
-  render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+  return (
+    <div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
-    );
-  }
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
 }
 
 export default Board;
